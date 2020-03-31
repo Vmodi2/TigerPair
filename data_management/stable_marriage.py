5f70dea46cd08154429f831e5bc8ef90b9a89ece
Chris Barkachi <cbarkachi@gmail.com>
2020-03-31 00:22:07 -0400

    Merge branch 'master' of https://github.com/Vmodi2/TigerPair

--- data_management/stable_marriage.py
+++ data_management/stable_marriage.py
@@ -1,4 +1,4 @@
-from database import get_rankings
+from database import get_rankings, Database
 
 weight_vector = (1, 3)
 
@@ -9,7 +9,32 @@ def get_ranking(student):
         return get_ranking.rankings[student]
     except:
         return None
+
+def get_rankings(weight_vector):
+    db = Database(app)
+    db.connect()
+    students = db.execute(selectall_query("students"), ())
+    alumni = db.execute(selectall_query("alumni"), ())
+    db.disconnect()
     
+    students_alumni = {}
+    for i in range(len(students)):
+        student_alumni = []
+        # assuming index 0 netid, index 1 is major, index 2 is career
+        for j in range(len(alumni)):
+            # can easily use this form to generalize to any number of features (columns will definitely change so keep an eye on range() especially)
+            score = sum(weight_vector[k] * (1 if students[i][k + 1] == alumni[j][k + 1] else 0) for k in range(len(students[i]) - 1))
+            student_alumni.append((alumni[j][0], score))
+        students_alumni[students[i][0]] = student_alumni
+
+    # sort rankings
+    for student in students_alumni:
+        students_alumni[student].sort(key=lambda x: x[1], reverse=True)
+
+    return students_alumni
+
+def selectall_query(table):
+    return f'SELECT * FROM {table}'
 
 if __name__ == '__main__':
     print(get_ranking("christine"))
--- database.py
+++ database.py
@@ -1,71 +1,45 @@
 import os
-import sqlite3
+import MySQLdb
 from sys import argv, stderr, exit
-from flask_mysqldb import MySQL
-from flask import Flask
-import yaml
 
-DB_NAME = "database.yaml"
+# from flaskext.mysql import MySQL
+# import yaml
+
 # app = Flask(__name__, template_folder='.')
 
 class Database():
-    def __init__(self, app):
-        if not os.path.isfile(DB_NAME):
-            raise Exception('Database connection failed')
-        self._dbname = DB_NAME
+    def __init__(self):
+        # if not os.path.isfile(DB_NAME):
+        #     raise Exception('Database connection failed')
+        # self._dbname = DB_NAME
         try:
-            db = yaml.load(open(self._dbname))
-            app.config['MYSQL_HOST'] = db["mysql_host"]
-            app.config['MYSQL_USER'] = db["mysql_user"]
-            app.config['MYSQL_PASSWORD'] = db["mysql_password"]
-            app.config['MYSQL_DB'] = db["mysql_db"]
-            self._app = app
+            # db = yaml.load(open(self._dbname))
+            self._args = ['localhost', 'TigerPair_dev', 'cos333', 'profiledb']
         except Exception as e:
             raise Exception('Configuration failed:', e)
     def connect(self):
         try:
-            self._connection = MySQL(self._app)
+            args = self._args
+            self._connection = MySQLdb.connect(host=args[0], user=args[1], passwd=args[2], db=args[3])
         except Exception as e:
             raise Exception('Connection failed:', e)
     def disconnect(self):
         self._connection.close()
-    def execute(self, query_string, params):
+    def execute_get(self, query_string, params):
         try:
             cursor = self._connection.cursor()
-            cursor.execute(query_string)
+            cursor.execute(query_string, params)
             self._connection.commit()
             result = cursor.fetchall()
             cursor.close()
+            return result
         except:
             self._connection.rollback()
-
-def selectall_query(table):
-    return f'SELECT * FROM {table}'
-
-# number of features
-N = 2
-def get_rankings(weight_vector):
-    db = Database(app)
-    db.connect()
-    students = db.execute(selectall_query("students"), ())
-    alumni = db.execute(selectall_query("alumni"), ())
-    
-    students_alumni = {}
-    for i in range(len(students)):
-        student_alumni = []
-        # assuming index 0 netid, index 1 is major, index 2 is career
-        for j in range(len(alumni)):
-            # can easily use this form to generalize to any number of features (columns will definitely change so keep an eye on range() especially)
-            score = sum(weight_vector[k] * (1 if students[i][k + 1] == alumni[j][k + 1] else 0) for k in range(len(students[i]) - 1))
-            student_alumni.append((alumni[j][0], score))
-        students_alumni[students[i][0]] = student_alumni
-
-    # sort rankings
-    for student in students_alumni:
-        students_alumni[student].sort(key=lambda x: x[1], reverse=True)
-
-    db.disconnect()
-    return students_alumni
-
-    if __name__ == '__main__':
-        pass
+    def execute_set(self, query_string, params):
+        try:
+            cursor = self._connection.cursor()
+            cursor.execute(query_string, params)
+            self._connection.commit()
+            cursor.close()
+        except:
+            self._connection.rollback()
