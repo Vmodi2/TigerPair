49e5623bbf769de8d5534e57a06a7b26f0da8ee0
vikashm <vikashm@princeton.edu>
2020-04-13 09:55:20 -0400

    Added all flask-login example files

--- TestFiles (Flask-Login examples)/models.py
+++ TestFiles (Flask-Login examples)/models.py
@@ -1,16 +1,17 @@
+"""Database models."""
 from . import db
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(UserMixin, db.Model):
-    """Model for user accounts."""
+    """User account model."""
 
     __tablename__ = 'flasklogin-users'
 
     id = db.Column(db.Integer,
                    primary_key=True)
-    name = db.Column(db.String,
+    name = db.Column(db.String(100),
                      nullable=False,
                      unique=False)
     email = db.Column(db.String(40),
---
+++ TestFiles (Flask-Login examples)/routes.py
@@ -0,0 +1,32 @@
+"""Logged-in page routes."""
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import current_user
+from flask import current_app as app
+from .assets import compile_auth_assets
+from flask_login import login_required, logout_user
+
+
+# Blueprint Configuration
+main_bp = Blueprint('main_bp', __name__,
+                    template_folder='templates',
+                    static_folder='static')
+compile_auth_assets(app)
+
+
+@main_bp.route('/', methods=['GET'])
+@login_required
+def dashboard():
+    """Serve logged-in Dashboard."""
+    return render_template('dashboard.jinja2',
+                           title='Flask-Login Tutorial.',
+                           template='dashboard-template',
+                           current_user=current_user,
+                           body="You are now logged in!")
+
+
+@main_bp.route("/logout")
+@login_required
+def logout():
+    """User log-out logic."""
+    logout_user()
+    return redirect(url_for('auth_bp.login'))
