c3f03d681cd85cfa19640eefef08da67b5d8d625
dmgoodman <43937598+dmgoodman@users.noreply.github.com>
2020-04-15 18:12:34 -0400

    Careers.js

--- README.md
+++ README.md
@@ -5,6 +5,19 @@ Functional Frontend - Daniel
 Understanding of CAS and Process API - Tara and Abhinaya
 DB Design, and MySql Implmentation - Chris and Vikash
 
+TODO:
+- Constant log-in state; make sure they're logged in even if redirected
+- Remember Me function
+- Make sure CAS is working - make sure Student Hub acts same as Alumni Hub
+- Make sure CAS is working on Admin profile
+- Check Admin Hub: 
+-- Make sure matching is working with all these new majors
+-- Get simple use cases to work
+- Front end hacking
+- Integrate with Glint
+- Make submit button feel like it's submitting
+- Change app.route names
+
 Completed for May 31st: 
 We have completed the first iteration of the alumni and user use cases, with a limited matching capability. 
 The user and alumni database is completed in a local and cloud instance (with a cpanel version in the works).
--- static/js/careers.js
+++ static/js/careers.js
@@ -1,15 +1,13 @@
-function update_career() {
-	career = ['Accounting', 'Advertising', 'Architecture/Planning', 'Building/Construction', 'Consulting', 
-	'Energy', 'Resources', 'Engr.-Chemical', 'Engr.-Civil', 'Engr.-Electrical', 'Engr.-Mech./AerospaceEngr.', 
-	'Engr-Other', 'Environmental Affairs', 'Finance', 'Health-Care'];
+function update_careers() {
+	careers = ["Accounting", "Advertising", "Architecture/Planning", "Building/Construction", "Consulting", 
+	"Energy", "Engr.-Chemical", "Engr.-Civil", "Engr.-Electrical", "Engr.-Mech./AerospaceEngr.", 
+	"Engr-Other", "Environmental Affairs", "Finance", "Health-Care"];
 
 	career_string = document.getElementById("career").innerHTML;
-	for (var i = 0; i < career.length; i++) {
-		career = career[i];
-		career_string += '<option value="' + career.toLowerCase() + '">' + career + '</option>';
+	for (var i = 0; i < careers.length; i++) {
+		career = careers[i];
+		career_string += '<option value="' + career + '">' + career + '</option>';
 	}
-
-	career_string += '<option value="other">Other</option>'
 
 	document.getElementById("career").innerHTML = career_string;
 }
