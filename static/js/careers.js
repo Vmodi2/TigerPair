a519c6951369a89c753b25e6a5458a78140137e5
vikashm <vikashm@princeton.edu>
2020-04-15 16:03:42 -0400

    ccareers

---
+++ static/js/careers.js
@@ -0,0 +1,15 @@
+function update_career() {
+	career = ['Accounting', 'Advertising', 'Architecture/Planning', 'Building/Construction', 'Consulting', 
+	'Energy', 'Resources', 'Engr.-Chemical', 'Engr.-Civil', 'Engr.-Electrical', 'Engr.-Mech./AerospaceEngr.', 
+	'Engr-Other', 'Environmental Affairs', 'Finance', 'Health-Care'];
+
+	career_string = document.getElementById("career").innerHTML;
+	for (var i = 0; i < career.length; i++) {
+		career = career[i];
+		career_string += '<option value="' + career.toLowerCase() + '">' + career + '</option>';
+	}
+
+	career_string += '<option value="other">Other</option>'
+
+	document.getElementById("major").innerHTML = career_string;
+}
