7bc2db0561830fbcce991d6f62f986e216d161aa
Chris Barkachi <cbarkachi@gmail.com>
2020-05-04 02:42:57 -0400

    fixed a bug with careers list

--- static/js/options.js
+++ static/js/options.js
@@ -4,25 +4,7 @@ $(document).ready(() => {
 });
 
 function update_careers() {
-    careers = ["Accounting", "Advertising", "Architecture/Planning", "Building/Construction", "Car\
-e-Other", "Care-Physical", "Consulting", "Energy Resources", "Engr-Chemical", "Engr\
--Civil", "Engr-Electrical", "Engr-Mech/Aerospace", "Engr-Other", "Environmental Aff\
-airs", "Fin-Asset Management", "Fin-Corporate Finance", "Fin-Financial Planning", "\
-Fin-Hedge Funds", "Fin-Investestment Banking", "Fin-Investment Management", "Fin-Pr\
-ivate Equity", "Finance-Commercial Banking", "Finance-Other", "Finance-Securities/C\
-ommodities", "Finance-Tax", "Finance-Venture Capital", "Foreign Service", "Fundrais\
-ing", "Gov-Cabinet Member", "Gov-Executive", "Gov-Legislator", "Gov-Other", "Gov-Po\
-licy Analysis", "Gov-Politics", "Gov-White House Staff", "Health Care-Mental", "Hea\
-lth", "Health", "Human", "Insurance", "Law-Corporate", "Law-Criminal", "Law-Intelle\
-ctual Property", "Law-Litigation", "Law-Other", "Law-Patent/Copyright", "Law-Tax", "\
-Law-Trusts and Estates", "Marketing", "Military", "Other", "Performing Arts", "Pri\
-nting/Publishing", "Public Relations", "Radio/TV/Film/Theater", "Real Estate", "Rel\
-igious Services", "Research & Development", "Resources", "Sales", "Social Work", "S\
-ports/Recreation", "Teaching-Arts", "Teaching-Humanities", "Teaching-Other", "Teach\
-ing-Science/Engr", "Teaching-Social Science", "Tech-Biotechnology", "Tech-E-Commerc\
-e", "Tech-Hardware", "Tech-Information Services/Systems", "Tech-Software Dev", "Tec\
-h-Telecommunications", "Technology-Other", "Transportation/Travel", "Trust & Estate\
-", "Veterinary Medicine", "Visual/Fine Arts", "Writing/Editing"];
+    careers = ["Accounting", "Advertising", "Architecture/Planning", "Building/Construction", "Care-Physical", "Consulting", "Energy Resources", "Engr-Chemical", "Engr-Civil", "Engr-Electrical", "Engr-Mech/Aerospace", "Engr-Other", "Environmental Affairs", "Fin-Asset Management", "Fin-Corporate Finance", "Fin-Financial Planning", "Fin-Hedge Funds", "Fin-Investestment Banking", "Fin-Investment Management", "Fin-Private Equity", "Finance-Commercial Banking", "Finance-Other", "Finance-Securities/Commodities", "Finance-Tax", "Finance-Venture Capital", "Foreign Service", "Fundraising", "Gov-Cabinet Member", "Gov-Executive", "Gov-Legislator", "Gov-Other", "Gov-Policy Analysis", "Gov-Politics", "Gov-White House Staff", "Health Care-Mental", "Health Care-Other", "Health", "Human", "Insurance", "Law-Corporate", "Law-Criminal", "Law-Intellectual Property", "Law-Litigation", "Law-Other", "Law-Patent/Copyright", "Law-Tax", "Law-Trusts and Estates", "Marketing", "Military", "Other", "Performing Arts", "Printing/Publishing", "Public Relations", "Radio/TV/Film/Theater", "Real Estate", "Religious Services", "Research & Development", "Resources", "Sales", "Social Work", "Sports/Recreation", "Teaching-Arts", "Teaching-Humanities", "Teaching-Other", "Teaching-Science/Engr", "Teaching-Social Science", "Tech-Biotechnology", "Tech-E-Commerce", "Tech-Hardware", "Tech-Information Services/Systems", "Tech-Software Dev", "Tech-Telecommunications", "Technology-Other", "Transportation/Travel", "Trust & Estate", "Veterinary Medicine", "Visual/Fine Arts", "Writing/Editing"];
 
     career_menu = document.getElementById("career");
     for (var i = 0; i < careers.length; i++) {
