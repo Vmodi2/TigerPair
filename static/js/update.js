93dea4c8847fefdcf519002c767828afef3fdd59
dmgoodman <43937598+dmgoodman@users.noreply.github.com>
2020-05-08 09:30:33 -0400

    Fix edit button + certificate bug

--- static/js/update.js
+++ static/js/update.js
@@ -1,11 +1,13 @@
-toggleEdit = function (className) {
+toggleEdit = function (className, className2) {
     return function () {
         $(className).attr('readonly', false);
         $(className).attr('disabled', false);
+        $(className).attr('hidden', false);
+        $(className2).attr('hidden', true);
         $(this).attr('hidden', true);
         $(this).next().attr('hidden', false);
     };
 };
 
-$('#edit-btn-1').on('click', toggleEdit('.info-1'));
-$('#edit-btn-2').on('click', toggleEdit('.info-2'));
+$('#edit-btn-1').on('click', toggleEdit('.info-1','.current-info-1'));
+$('#edit-btn-2').on('click', toggleEdit('.info-2','.current-info-2'));
