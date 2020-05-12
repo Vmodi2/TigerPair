a4f34b87cc0241a3b40638ee5ebc6d978a2bb2a1
Christopher Barkachi <cbarkachi@gmail.com>
2020-05-12 16:23:54 -0400

    demo

--- static/js/delete_account.js
+++ static/js/delete_account.js
@@ -1,7 +1,7 @@
 let a = document.querySelector('#delete-account');
 a.addEventListener('click', function (ev) {
     ev.preventDefault();
-    let response = confirm('Are you sure you would like to permanently delete this account? If so, press ok to confirm.');
+    let response = confirm('Are you sure you would like to permanently delete this account?');
     if (response) {
         window.location.assign($(a).attr('href'));
     }
